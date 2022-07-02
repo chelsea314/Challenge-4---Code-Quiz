e86e14d3a8d7acca544ee4d1443353f1d8b6a67d
Chelsea Sexton <ChelseSsexton@Chelseas-MBP.lan>
2022-07-01 22:33:07 -0500

    button values assigned

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-// welcome page with start button    
-//            :hover; targets; on.click  
+// Assigns html elements to variables
 var timeEl = document.querySelector('.time');
 var startButton = document.querySelector('#start-button');
 var welcomeScreen = document.querySelector('#welcome-screen');
@@ -8,11 +7,12 @@ var btnB = document.querySelector("#choiceB");
 var btnC = document.querySelector("#choiceC");
 var btnD = document.querySelector("#choiceD");
 
+// Array of questions, answer choices & correct answers
 var questionArr = [
     {
         question: "Which one is NOT a Javascript data type?",
-        choices: ["Number", "Function", "String", "Boolean"],
-        crcAns: "Function"
+        choices: ["Number", "Cats", "String", "Boolean"],
+        crcAns: "Cats"
     },
     {
         question: "What does NaN stand for?",
@@ -35,9 +35,13 @@ var questionArr = [
         crcAns: "All of the Above"
     },
 ]
+console.log('questionArr length: ' + questionArr.length);
 
+// Currect Question variable
 var currentQ;
 
+// When you click the start button, welcome screen will disappear, current Q will appear. 
+// Timer will start.
 function start(){
     document.getElementById('welcome-screen').style.display = "none";
     document.getElementById('questions').setAttribute("class", "");
@@ -46,53 +50,92 @@ function start(){
     setTime();
 }
 
+// Sets the question content to the questionsArr questions content
+// Sets text content of buttons to the questionsArr choices content
+// Sets the value of each button to the string in the questionsArr choices
 function showQuestion() {
     document.getElementById("question").textContent = questionArr[currentQ].question
 
     btnA.textContent = questionArr[currentQ].choices[0];
+    btnA.value = questionArr[currentQ].choices[0];
     btnB.textContent = questionArr[currentQ].choices[1];
+    btnB.value = questionArr[currentQ].choices[1];
     btnC.textContent = questionArr[currentQ].choices[2];
+    btnC.value = questionArr[currentQ].choices[2];
     btnD.textContent = questionArr[currentQ].choices[3];
+    btnD.value = questionArr[currentQ].choices[3];
 }
 
-
-
+//Sets timer starting point 
 var secondsLeft = 45;
 
+// Timer
 function setTime() {
+    // Sets interval in variable
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft + " seconds left"
 
-        if (secondsLeft <= 0) {
+        if (secondsLeft <= 0 || currentQ + 1 >= questionArr.length) {
+            // Stops execution of action at set interval
             clearInterval(timerInterval);
+            // Timer displays new text: 'Quiz Complete'
+            timeEl.textContent = "Quiz Complete"
+            // Calls function to input user initials
             saveInitials();
         }
     }, 1000);
 }
 
+// Score starts at 0
 var score = 0;
 
-function handleAnswer(event) {
-    // if correct do something, if incorrect something else;
-    
-    if (event.taget === questionArr[currentQ].crcAns){
-        score + 10; 
+// When an answer choice is clicked,  
+function handleAnswer(event) {    
+    // if the click's value is equal to the value of the crcans (correct answer) within the questionArr of the current question, add 10 points to the score and display 'correct!'
+    if (event.target.value === questionArr[currentQ].crcAns){
+        score += 10; 
         document.getElementById('response').setAttribute("class", '');
+        document.getElementById('response').innerHTML = "Correct!";
+        // allDone();
+        // console.log('CurrentQ: ' + currentQ);
+        // console.log(event.target);
+    // if not equal, subtract 10 points to the score, deduct 10 seconds from the timer and display 'incorrect!'    
     } else {
         secondsLeft -= 10;
         score -=10;
-        document.getElementById('timer').innerHTML = secondsLeft;};
-        let text = document.getElementById('response').innerHTML;
         document.getElementById('response').setAttribute("class", '');
-        document.getElementById('response').innerHTML = text.replace("Correct!", "Incorrect!");
-
+        document.getElementById('response').innerHTML = "Incorrect!";
+        document.getElementById('timer').innerHTML = secondsLeft;
+        // allDone();
+        console.log("currentQ: " + currentQ);
+        console.log(event.target);
+    } 
     
-    console.log(score);
-    console.log(event.target)
 
+    console.log("currentQ: " + currentQ); 
+    // console.log('element reference: ', event.target);
+    // console.log('real value in click handler: ', event.target.value);
+    console.log('score: ', score);
+    
+
+    // Increase currentQ's index by 1 
     currentQ++;
-    showQuestion()
+    // run function showQuestion (again)
+    allDone()
+    // showQuestion()
+    console.log('------------');
+}
+
+function allDone(){
+   if (currentQ + 1 >= questionArr.length){
+         console.log('if questionArr length: ' + questionArr.length);
+         var secondsLeft = 0;
+        saveInitials();
+    } else {
+        console.log('else questionArr length: ' + questionArr.length);
+        showQuestion();
+    }
 }
 
 startButton.addEventListener("click", start)
@@ -100,6 +143,9 @@ btnA.addEventListener("click", handleAnswer)
 btnB.addEventListener("click", handleAnswer)
 btnC.addEventListener("click", handleAnswer)
 btnD.addEventListener("click", handleAnswer)
+
+
+
 
 
 var inputEl = document.getElementById("input");
@@ -125,6 +171,8 @@ var userInfo = {
 }
 
 function showHighScores(){
+
+    
     document.getElementById('insertInitials').setAttribute('class', 'hidden');
     document.getElementById('highScores').setAttribute('class', '');
 
@@ -137,6 +185,22 @@ function showHighScores(){
     
 
 
+
+/////////////////////////////
+// Tests
+// btnA.addEventListener("click", handleAnswer)
+// btnB.addEventListener("click", handleAnswer)
+// btnC.addEventListener("click", handleAnswer)
+// btnD.addEventListener("click", function (elem){
+//     if (elem.data-value === questionArr[currentQ].choices[3]);
+// }
+// );
+
+
+
+
+
+//////////////////////////////
 //        timer begins on click; welcome screen hides  
 //          
 //        question appears - ordered list - multiple choice    
